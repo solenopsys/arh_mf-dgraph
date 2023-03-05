6abd454782326d813576557005ff11d858072c68
AlexStorm <info@alexstorm.ru>
2023-03-05 09:23:36 +0530

    fix names

--- src/app/remote-entry/entry.component.ts
+++ src/app/remote-entry/entry.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'hyperconverged-dgraph-entry',
+  selector: 'sc-dgraph-entry',
   template: `<div class="remote-entry">
     <h2>dgraph's Remote Entry Component</h2>
   </div>`,
